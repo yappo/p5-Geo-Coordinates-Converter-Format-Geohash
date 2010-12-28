a56c0fd919a88b84be891a9fe6a7e17fcbd07c0e
Kazuhiro Osawa <yappo@shibuya.pl>
2010-12-28 16:01:59 +0900

    initial commit

---
+++ lib/Geo/Coordinates/Converter/Format/GeoHash.pm
@@ -0,0 +1,32 @@
+package Geo::Coordinates::Converter::Format::GeoHash;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Geo::Coordinates::Converter::Format::GeoHash -
+
+=head1 SYNOPSIS
+
+  use Geo::Coordinates::Converter::Format::GeoHash;
+
+=head1 DESCRIPTION
+
+Geo::Coordinates::Converter::Format::GeoHash is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
